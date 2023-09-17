5abf1904ffa1bd32e004a248629b6b8d9fc7c24f
Abhishek Sagar <sachinites@gmail.com>
2023-09-17 06:52:13 +0000

    Bug fix, Code Refactoring. Added test file.

--- compile.sh
+++ compile.sh
@@ -3,5 +3,6 @@ gcc -g -c lex.yy.c -o lex.yy.o
 gcc -g -c MExpr.c -o MExpr.o
 gcc -g -c stack/stack.c -o stack/stack.o
 gcc -g -c ExpressionParser.c -o ExpressionParser.o
-gcc -g MExpr.o lex.yy.o stack/stack.o ExpressionParser.o -o exe -lfl -lm
+gcc -g -c test.c -o test.o
+gcc -g MExpr.o lex.yy.o stack/stack.o test.o ExpressionParser.o -o exe -lfl -lm
 # run the ./exe 
